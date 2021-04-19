a36c78072ba4ff2385ac4cb145b9214e54a6f86c
wassim ben slimene <wassim.benslimene@supcom.tn>
2021-04-19 17:33:46 +0100

    Update commons.ts

--- src/app/commons.ts
+++ src/app/commons.ts
@@ -8,5 +8,5 @@ export function hideLoading() {
 
 export function doNothing () {}
 
-export const webServiceServer: string = 'http://3.85.41.219:8080';
+export const webServiceServer: string = 'http://3.87.61.209:31200';
 export const webServiceEndpoint: string = webServiceServer + '/personapp/api';
